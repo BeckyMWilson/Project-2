f76fc87bf12e2b5ca069238a0992787656f3e8d4
Bruno Marcenaro <bems2588@gmail.com>
2022-04-11 13:09:09 -0400

    fix route

--- controllers/gameUser.js
+++ controllers/gameUser.js
@@ -3,22 +3,34 @@ const db = require("../models/Game");
 module.exports = function () {
   return {
     getAllGames: function (req, res) {
-      db.findAll().then(function (gamesData) {
-        console.log("GET all game", gamesData)
-        res.json(gamesData);
-      });
+      if (req.session.user) {
+        db.findAll().then(function (gamesData) {
+          console.log("GET all game", gamesData)
+          res.json(gamesData);
+        });
+      } else {
+        res.redirect("/")
+      }
     },
     getSingleGame: function (req, res) {
-      db.findOne({ where: { id: req.params.userid } }).then(function (gamesData) {
-        console.log("GET all users", gamesData)
-        res.json(gamesData);
-      });
+      if (req.session.user) {
+        db.findOne({ where: { id: req.params.userid } }).then(function (gamesData) {
+          console.log("GET all users", gamesData)
+          res.json(gamesData);
+        });
+      } else {
+        res.redirect("/");
+      }
     },
     newGame: function (req, res) {
-      db.create(req.body).then(function (gamesData) {
-        console.log("GET all users", gamesData)
-        res.json(gamesData);
-      });
+      if (req.session.user) {
+        db.create(req.body).then(function (gamesData) {
+          console.log("GET all users", gamesData)
+          res.json(gamesData);
+        });
+      } else {
+        res.redirect("/");
+      }
     },
   }
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -4,22 +4,36 @@ const db = require("../models/user");
 module.exports = function () {
   return {
     getAllUser: function (req, res) {
-      db.findAll().then(function (userData) {
-        console.log("GET all users", userData)
-        res.json(userData);
-      });
+      if (req.session.user) {
+        db.findAll().then(function (userData) {
+          console.log("GET all users", userData)
+          res.json(userData);
+        });
+      } else {
+        res.redirect("/")
+      }
     },
+
     getSingleUser: function (req, res) {
-      db.findOne({ where:{ id: req.params.userid}}).then(function (userData) {
-        console.log("GET all users", userData)
-        res.json(userData);
-      });
+      if (req.session.user) {
+        db.findOne({ where: { id: req.params.userid } }).then(function (userData) {
+          console.log("GET all users", userData)
+          res.json(userData);
+        });
+      } else {
+        res.redirect("/")
+      }
     },
     newUser: function (req, res) {
-      db.create(req.body).then(function (userData) {
-        console.log("GET all users", userData)
-        res.json(userData);
-      });
+      if (req.session.user) {
+        db.create(req.body).then(function (userData) {
+          console.log("GET all users", userData)
+          res.json(userData);
+        });
+      } else {
+        res.redirect("/")
+
+      }
     },
   }
 }
